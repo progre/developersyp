797e74055fc9477085f947a644ebffc46121ebb3
progre <djyayutto@gmail.com>
2014-03-23 19:43:32 +0900

    fix: toplevel directory

--- init.bat
+++ init.bat
@@ -1,15 +1,10 @@
 @echo off
+echo ジャンクションを作成します
 if not exist http\src\common (
   mklink /j http\src\common root\src\common
 )
 if not exist http\src\public\scripts\common (
   mklink /j http\src\public\scripts\common root\src\common
 )
-echo   npm install (/)
-call npm install
-echo   npm install (http)
-cd http && call npm install && cd ..
-echo   npm install (root)
-cd root && call npm install && cd ..
 echo.
-echo Complete!
+echo 完了
